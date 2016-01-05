cc06c169d0d46458a8bf780f283c1ce04601d77d
James Speirs <james.speirs@gmail.com>
2016-01-05 13:41:44 -0700

    Add is.date.valid function and tests

--- test/index.js
+++ test/index.js
@@ -266,6 +266,8 @@ test('is.date', function (t) {
   var F = function () {};
   F.prototype = new Date();
   t.notOk(is.date(new F()), 'Date subtype is not date');
+  t.ok(is.date.valid(new Date()), 'new Date() is a valid date');
+  t.notOk(is.date.valid(new Date('')), 'new Date("") is not a valid date');
   t.end();
 });
 
