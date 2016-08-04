30056bafe6a9b2d476cd10f13745997fc11d720e
Brian Wendt <whiskeredwonder@users.noreply.github.com>
2016-08-04 14:24:02 -0500

    Updates to tests based on feedback

--- test/index.js
+++ test/index.js
@@ -636,8 +636,8 @@ test('is.symbol', function (t) {
 });
 
 test('is.oneOf', function (t) {
-  t.ok(is.oneOf('foo', 'object', 'array', 'string'), 'oneOf returns true when a match is found');
-  t.notOk(is.oneOf('foo', 'number', 'fn', 'regexp'), 'oneOf returns false when no match is found');
-  t.throws(function() { is.oneOf('foo') }, /err: no types/i, 'throws when called with no types');
+  t.ok(is.oneOf('foo', ['object', 'array', 'string']), 'oneOf returns true when a match is found');
+  t.notOk(is.oneOf('foo', ['number', 'fn', 'regexp']), 'oneOf returns false when no match is found');
+  t.throws(function() { is.oneOf('foo') }, /array with at least one type/i, 'throws when called with no types');
   t.end();
 });
